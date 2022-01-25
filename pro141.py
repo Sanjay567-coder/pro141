37ea28347513ad32c73afa383294cac38bcff253
Sanjay Srinivas <72558978+Sanjay567-coder@users.noreply.github.com>
2022-01-25 18:44:28 +0530

    Add files via upload

---
+++ pro141.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, Request
+import csv
+
+all_articles = []
+
+with open('articles.csv', encoding='utf-8')as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_articles = data[1:]
+
+liked_articles = []
+not_liked_articles = []
+
+app = Flask(__name__)
+
+@app.route('/get-articles')
+def get_articles():
+    return jsonify({
+        'data' : all_articles[0],
+        'status' : 'success'
+    })
+
+@app.route("/liked-articles", methods=["POST"])
+def liked_movie():
+    global all_articles
+    movie = all_articles[0]
+    all_articles = all_articles[1:]
+    liked_articles.append(movie)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/unliked-articles", methods=["POST"])
+def unliked_movie():
+    global all_articles
+    movie = all_articles[0]
+    all_articles = all_articles[1:]
+    not_liked_articles.append(movie)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+if __name__ == '__main__':
+    app.run()
